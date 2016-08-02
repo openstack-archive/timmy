openstack-archive/timmy

commit 7f1e4ddf690087097e1ac5a6f925e693f277d7b4

Implement support of Fuel 9.1 (10.0) for API (CLI not fixed yet)

timmy/nodes.py
@@ -31,15 +31,17 @@ from tools import w_list, run_with_lock
 from copy import deepcopy
 
 try:
-    from fuelclient.client import Client as FuelClient
+    import fuelclient.client
+    if type(fuelclient.client.APIClient) is type:
+        # fuel 9.1+ (originally 10.0+)
+        from fuelclient.client import APIClient as FuelClient
+        FUEL_10 = True
+    elif type(fuelclient.client.APIClient) is fuelclient.client.Client:
+        # fuel 9.0 and below
+        from fuelclient.client import Client as FuelClient
+        FUEL_10 = False
 except:
     FuelClient = None
-
-try:
-    from fuelclient.client import logger
-    logger.handlers = []
-except:
-    pass
 
 
 class Node(object):
@@ -438,11 +440,19 @@ class NodeManager(object):
                     os.environ['https_proxy'] = ''
                     os.environ['http_proxy'] = ''
                 self.logger.info('Setup fuelclient instance')
-                self.fuelclient = FuelClient()
-                self.fuelclient.username = self.conf['fuel_user']
-                self.fuelclient.password = self.conf['fuel_pass']
-                self.fuelclient.tenant_name = self.conf['fuel_tenant']
-                # self.fuelclient.debug_mode(True)
+                if FUEL_10:
+                    self.fuelclient = FuelClient(
+                        host=self.conf['fuel_ip'],
+                        port=self.conf['fuel_port'],
+                        os_username=self.conf['fuel_user'],
+                        os_password=self.conf['fuel_pass'],
+                        os_tenant_name=self.conf['fuel_tenant'])
+                else:
+                    self.fuelclient = FuelClient()
+                    self.fuelclient.username = self.conf['fuel_user']
+                    self.fuelclient.password = self.conf['fuel_pass']
+                    self.fuelclient.tenant_name = self.conf['fuel_tenant']
+                    # self.fuelclient.debug_mode(True)
             except Exception as e:
                 self.logger.info('Failed to setup fuelclient instance:%s' % e,
                                  exc_info=True)

timmy/conf.py
@@ -31,6 +31,7 @@ def load_conf(filename):
     conf['env_vars'] = ['OPENRC=/root/openrc', 'IPTABLES_STR="iptables -nvL"']
     conf['fuel_ip'] = '127.0.0.1'
     conf['fuel_user'] = 'admin'
+    conf['fuel_port'] = '8000'
     conf['fuel_pass'] = 'admin'
     conf['fuel_tenant'] = 'admin'
     conf['fuelclient'] = True  # use fuelclient library by default

timmy/env.py
@@ -16,7 +16,7 @@
 #    under the License.
 
 project_name = 'timmy'
-version = '1.9.0'
+version = '1.10.1'
 
 if __name__ == '__main__':
     exit(0)

commit a339a2614eb8c74525cd069a86602ff5b3e9e78f

Fix regression introduced in 7f1e4ddf690087097e1ac5a6f925e693f277d7b4, fix flake8

timmy/nodes.py
@@ -43,6 +43,12 @@ try:
 except:
     FuelClient = None
 
+try:
+    from fuelclient.client import logger
+    logger.handlers = []
+except:
+    pass
+
 
 class Node(object):
     ckey = 'cmds'
@@ -326,7 +332,7 @@ class Node(object):
 
         for item in self.logs:
             start_str = None
-            if 'start' in item or hasattr(self,'logs_days'):
+            if 'start' in item or hasattr(self, 'logs_days'):
                 if hasattr(self, 'logs_days') and 'start' not in item:
                     start = self.logs_days
                 else: