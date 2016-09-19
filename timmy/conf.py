03def584a981ef6769f89a20296815aaa5ee84f9
Dmitry Sutyagin <dsutyagin@mirantis.com>
2016-09-19 18:53:41 +0300

    Change: iptables collection, remove iptables from env
    Change-Id: I61814b92ad488c214f642a30a0a7636cd8db04f7

--- timmy/conf.py
+++ timmy/conf.py
@@ -28,8 +28,7 @@ def load_conf(filename):
     conf['ssh_opts'] = ['-oConnectTimeout=2', '-oStrictHostKeyChecking=no',
                         '-oUserKnownHostsFile=/dev/null', '-oLogLevel=error',
                         '-lroot', '-oBatchMode=yes']
-    conf['env_vars'] = ['OPENRC=/root/openrc', 'IPTABLES_STR="iptables -nvL"',
-                        'LC_ALL="C"', 'LANG="C"']
+    conf['env_vars'] = ['OPENRC=/root/openrc', 'LC_ALL="C"', 'LANG="C"']
     conf['fuel_ip'] = '127.0.0.1'
     conf['fuel_api_user'] = 'admin'
     conf['fuel_api_pass'] = 'admin'
