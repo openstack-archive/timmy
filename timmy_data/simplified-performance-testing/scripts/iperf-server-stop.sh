779a896dcb8d38d81fe36daaf732d4951238fcba
Dmitry Sutyagin <dsutyagin@mirantis.com>
2017-02-24 14:41:01 -0800

    Fix: exec_pair trace; SPT bugs; print non-0 exit
    - Fix crash when hitting logger.warning in exec_pair
    - Fix SPT bugs:
      - iperf sometimes not installed for HW to HW test
      - iperf_server_stop broken in several ways
      - glance download script has wrong filename
      - SPT_parser traces on empty glance output
    - Print to stdout on non-0 exit to tell about unclean exit to user
    - Reorder SPT copy-paste line once more
    
    Change-Id: I7a37b5fce5bc620cc9e2db8d232180c215cd3782

--- timmy_data/simplified-performance-testing/scripts/iperf-server-stop.sh
+++ timmy_data/simplified-performance-testing/scripts/iperf-server-stop.sh
@@ -2,11 +2,11 @@
 
 SPT_IPERF_PORT=${SPT_IPERF_PORT:-"65432"}
 
-killall iperf
-while ["$(iptables -L --line-numbers | grep -c 'spt-temporary-rule')" -ge 0 ]
+killall -9 iperf
+while [ "$(iptables -L --line-numbers | grep -c 'spt-temporary-rule')" -gt 0 ]
 do
   rulenum=`iptables -L --line-numbers | grep 'spt-temporary-rule' | head -n 1 | awk '{print $1}'`
-  [ -n "$sulenum" ] && [ "$rulenum" -ge 0 ] && iptables -D INPUT $rulenum
+  [ -n "$rulenum" ] && [ "$rulenum" -ge 0 ] && iptables -D INPUT $rulenum
 done
 if [ -n $SERVER_OUTPUT ]
 then
